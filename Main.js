6d0d1ee978ce80f24ac9f3e8d4f0f6cc2cf82a2f
Steve <lawsons@lawsons-HP-Z800-Workstation.(none)>
2012-12-31 15:22:38 -0500

    Fixed inheritance for activity

--- Main.js
+++ Main.js
@@ -1,1 +1,23 @@
-var testActivity = new Backbone.Android.Activity();
+var testActivity = new Backbone.Android.Activity({
+	onCreate: function() {
+		console.log('In custom onCreate');
+		
+		var grid1 = new Backbone.Android.Widget.Grid();
+		var grid2 = new Backbone.Android.Widget.Grid();
+		
+	}
+});
+
+console.log(testActivity);
+
+var testActivity2 = new Backbone.Android.Activity({
+	onCreate: function() {
+		console.log('In custom onCreate');
+		
+		var grid1 = new Backbone.Android.Widget.Grid();
+		var grid2 = new Backbone.Android.Widget.Grid();
+		
+	}
+});
+
+console.log(testActivity2);
